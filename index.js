16b02e64f6d1948e3ebfa1d08698f475c06e426c
Md Mahabur Alam <mahaburcse@gmail.com>
2024-10-07 15:10:58 +0600

    create node js server

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 /**
-    Title: Uptime Monitoring Application
-    Description: A restful API to monitor up or down time of user defined links
-*/
+ * create server
+ * 
+ */
 
-// dependencies
+// import required modules
 const http = require('http');
-const url = require('url');
-// console.log(http);
 
-// app object - module scaffolding
-const app = {}; //
+// app module or scaffolding
+const app = {};
 
 // configuration
-app.config = {
-    port: 3000
-};
+app.config = {port: 3000};
 
 // create server
 app.createServer = () => {
-    const server = http.createServer( app.handleReqRes );
-    server.listen(app.config.port, () => {
-        console.log(`Listening to port $  {app.config.port}`);
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () =>{
+        console.log(`Node js server run in port : ${app.config.port}`);
     });
-}
 
-// handle request response
-app.handleReqRes = (req, res) => {
-    // request handle
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname; // if pass param or any special symbol in url not showing in console
-    const trimmedPath = path.replace(/^\/+|\/+$/g, ''); //if pass any special symbol in url not showing in console
-
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedUrl.query; // received or show params value as an object
-    const headerObject = req.headers; // show all header information
-    console.log(headerObject);
-
-    // response handle
-    res.end('Hello Node js developer!!');
 };
 
-// start the server
+// handle request and response
+app.handleReqRes = (req, res) => {
+    res.end(`create node js server`);
+}
+
 app.createServer();
