3a8c5757c11e09844ff57ad86d0d12e970b1135c
Md Mahabur Alam <mahaburcse@gmail.com>
2024-10-08 18:51:07 +0600

    code re structure

---
+++ helpers/handleReqRes.js
@@ -0,0 +1,49 @@
+// import modules
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
+
+// app module or scaffolding
+const handler = {};
+
+handler.handelReqRes = (req, res) => {
+
+    // get full url
+    const getFullUrl = url.parse(req.url, true);
+
+    // get path name
+    const pathName = getFullUrl.pathname;
+
+    // pares path name
+    const trimmedPathName = pathName.replace(/^\/+|\/+$/g,'');
+    // console.log(pathName, trimmedPathName);
+
+    // get query string or param value
+    const getQueryString = getFullUrl.query;
+    // console.log(getQueryString);
+
+    // get method name
+    const getMethod = req.method.toLowerCase();
+    // console.log(getMethod);
+
+    // get headers information
+    const headers = req.headers;
+    // console.log(req);
+    
+
+    // get body data
+    // create an object of class
+    const dataDecoder = new StringDecoder('utf-8');
+
+    let data = '';
+    req.on('data', (buffer) => {
+        data += dataDecoder.write(buffer);
+    });
+
+    req.on('end', ()=>{
+        data += dataDecoder.end();
+        res.end(`Node js is awesome and power full..`);
+        console.log(data);
+    });
+    
+    // console.log(data);
+};
--- index.js
+++ index.js
@@ -10,8 +10,7 @@
 
 // import required module
 const http = require('http');
-const url = require('url');
-const {StringDecoder} = require('string_decoder')
+const {handleReqRes} = require('./helpers/handleReqRes')
 
 // app module or scaffolding
 const app = {};
@@ -30,38 +29,7 @@ app.createServer = () => {
 
 // handle request and response
 // handle request and response
-app.handleReqRes = (req, res) => {
-
-    // get full url
-    const getFullUrl = url.parse(req.url, true);
-
-    // get path name
-    const pathName = getFullUrl.pathname;
-
-    // pares path name
-    const trimmedPathName = pathName.replace(/^\/+|\/+$/g,'');
-    // console.log(pathName, trimmedPathName);
-
-    // get query string or param value
-    const getQueryString = getFullUrl.query;
-    // console.log(getQueryString);
-
-    // get method name
-    const getMethod = req.method.toLowerCase();
-    // console.log(getMethod);
-
-    // get headers information
-    const headers = req.headers;
-    // console.log(req);
-    
-
-    // get body data
-    // create an object of class
-    const data = new StringDecoder('utf-8');
-    
-    // console.log(data);
-    res.end(`Node js is awesome and power full..`);
-};
+app.handleReqRes = handleReqRes;
 
 // console.log(url);
 
